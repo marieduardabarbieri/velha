1a1aa83099c51739e129be0362d5307c33110c5e
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2023-08-30 20:40:24 -0400

    função comecar jogo criada

--- js/main.js
+++ js/main.js
@@ -5,4 +5,29 @@ window.onload = () => {
     }
 }
 
-let JogadorVez =  document.querySelector(".JogadorVez");
+let JogadorVez =  document.querySelector(".JogadorVez"); //Retorna o primeiro elemento dentro do documento
+
+let seleciona;
+let x = "X";
+
+let posicoesPossiveis =[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,5,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [3,5,7]
+]
+
+function comecar(){
+    seleciona = [];
+
+    JogadorVez.innerHTML = `Jogador é: ${x}`;
+
+    document.querySelectorAll(".JogoVelha button").forEach((item) => { //seleciona tudos os botoes da div Jogovelha 
+      item.innerHTML = "";  //inicia com o innerHTML vazio, pq ninguem comecou
+      item.addEventListener("click", newMove)
+    })
+}
