2260632d5e7012150ae41484d0c0057f826797fe
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2023-09-02 16:28:54 -0400

    estilização

--- js/main.js
+++ js/main.js
@@ -5,66 +5,58 @@ window.onload = () => {
     }
 }
 
-let JogadorVez =  document.querySelector(".JogadorVez"); //Retorna o primeiro elemento dentro do documento
-
-let seleciona;
-let x = "X";
-
 let posicoesPossiveis = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,5,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [3,5,7],
+    [1,2,3],  [4,5,6], 
+    [7,8,9],  [1,5,9], 
+    [1,4,7],  [2,5,8],
+    [3,6,9],  [3,5,7],
 ];
 
+let JogadorVez =  document.querySelector(".JogadorVez"); //Retorna o primeiro elemento dentro do documento
+let clica;
+let x = "X";
+
 function comecar(){
-    seleciona = [];
-
+    clica = [];
     JogadorVez.innerHTML = `Quem joga é: ${x}`;
-
-    document.querySelectorAll(".JogoVelha button").forEach((item) => { //seleciona tudos os botoes da div Jogovelha 
-      item.innerHTML = "";  //inicia com o innerHTML vazio, pq ninguem comecou
-      item.addEventListener("click", newMove)
+    document.querySelectorAll(".JogoVelha button").forEach((quadrados) => { //seleciona tudos os botoes da div Jogovelha 
+        quadrados.innerHTML = "";  //inicia com o innerHTML vazio, pq ninguem comecou
+        quadrados.addEventListener("click", newMove)
     })
-}
+}comecar();
 
-comecar();
 
 function newMove(e) { //função dos botoões
     const index = e.target.getAttribute("id");
     e.target.innerHTML = x;
     e.target.removeEventListener("click", newMove);
-    seleciona[index] = x;
-
-    setTimeout(() => {
-        check();
-    }, [100]);
+    clica[index] = x;
 
     x = x === "X" ? "O" : "X";
     JogadorVez.innerHTML = `Quem joga é: ${x}`;
+
+    setTimeout(() => {
+        checaQuad();
+    });
 }
 
-function check() { //função de checar, revisa quem jogou por ultimo e mostra caso de empate
+function checaQuad() { //função de checar, revisa quem jogou por ultimo e mostra caso de empate
     let ultimoJogador = x === "X" ? "O" : "X";
 
-    const quadradinhos = seleciona
-    .map((item, i) => [item, i])
-    .filter((item) => item[0] === ultimoJogador)
-    .map((item) => item[1])
+    const quadradinhos = clica
+    .map((quadrados, i) => [quadrados, i])
+    .filter((quadrados) => quadrados[0] === ultimoJogador)
+    .map((quadrados) => quadrados[1])
 
     for(pos1 of posicoesPossiveis) { //for de verificação da vez do jogador
-        if(pos1.every((item) => quadradinhos.includes(item))){
+        if(pos1.every((quadrados) => quadradinhos.includes(quadrados))){
         alert("O Jogador " + ultimoJogador + " venceu!!");
         comecar();
         return;
         }
     }
 
-    if(seleciona.filter((item) => item).length === 9){//caso de empate
+    if(clica.filter((quadrados) => quadrados).length === 9){//caso de empate
         alert("Empatou, comece de novo!!");
         comecar();
         return;
